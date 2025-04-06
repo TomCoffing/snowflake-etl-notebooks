f07d7b5b73bbfd09a3db402aeb16786fd90d0c15
TomCoffing <tom.coffing@coffingdw.com>
2025-04-06 09:55:45 -0400

    Update cluster_keys.sql

--- DDL/cluster_keys.sql
+++ DDL/cluster_keys.sql
@@ -1,3 +1,3 @@
 ALTER TABLE SQL_CLASS.CLAIMS CLUSTER BY (CLAIM_DATE);
 ALTER TABLE SQL_CLASS.ADDRESSES CLUSTER BY (SUBSCRIBER_NO);
-ALTER TABLE SQL_CLASS.SUBSCRIBERS CLUSTER BY (SUBSCRIBER_NO); -- added a comment to say these tables join on Subscriber_no
+ALTER TABLE SQL_CLASS.SUBSCRIBERS CLUSTER BY (SUBSCRIBER_NO); -- added a comment to say addresses and subscribers join on Subscriber_no
